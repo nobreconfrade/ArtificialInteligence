c6ee71a4f7a3e1ea8dcd77e41d9a958b2708508e
William Pereira <willpereirabr@gmail.com>
2017-03-21 15:15:12 -0300

    [show_grid] problem: it doesn't show it

--- ants.rb
+++ ants.rb
@@ -5,8 +5,7 @@ class Ants < AntsMethods
   maxc = 30
   maxr = 30
   methods = AntsMethods.new
-  puts "|---> Max Length=#{maxc} |---> Max Height #{maxr}}"
-  grid = methods.populate_grid(maxc,maxr)
-  methods.show_grid(grid,maxc,maxr)
+  grid = methods.populate_grid(maxr,maxc)
+  methods.show_grid(grid,maxr,maxc)
 
 end
--- antsmethods.rb
+++ antsmethods.rb
@@ -1,6 +1,16 @@
 class AntsMethods
   def dead_ants(grid,maxr,maxc)
-
+    deadants = 0
+    while (deadants != maxr*maxc/10)
+      key = rand(0..maxr) * maxr + rand(0..maxc)
+      if grid["#{key}"] == "2"
+        next
+      else
+        grid = {"#{key}" => "2"}
+        puts grid["#{key}"]
+      end
+      deadants += 1
+    end
   end
 
   def live_ants(grid)
@@ -8,27 +18,40 @@ class AntsMethods
   end
 
   def populate_grid(maxr,maxc)
-    # NOTE: '0' for nothing
+    # NOTE: ' ' for nothing
     # =>    '1' for live ants
     # =>    '2' for dead ants
-    grid = Hash.new(" ")
-    grid = dead_ants(grid)
-    grid = live_ants(grid)
+    grid = Hash.new
+    for r in 0..maxr do
+      for c in 0..maxc do
+        key = r * maxr + c
+        grid = {"#{key}" => "0"}
+        puts grid["#{key}"]
+      end
+    end
+    show_grid(grid,maxr,maxc)
+    grid = {"450" => "2"}
+    show_grid(grid,maxr,maxc)
+    grid = dead_ants(grid,maxr,maxc)
+    # grid = live_ants(grid)
     return grid
   end
-
-  def show_grid (grid,maxc,maxr)
+  # FIXME: what is the problem with this method? It doesn't print the grid, it even "forgets" the values
+  def show_grid (grid,maxr,maxc)
     # NOTE: Next line clear the ubuntu terminal and them wait X seconds
-    system 'clear'
-    sleep(5)
-    for i in 0...maxc do
-      for j in 0...maxr do
+    # system 'clear'
+    print grid["1"]
+    for r in 0...maxr do
+      for c in 0...maxc do
+        key = r * maxr + c
         print "|"
-        print grid["#{i * maxc + j}"]
-        if j == maxc-1
+        print grid["#{key}"]
+        if c == maxc-1
           puts ""
         end
       end
     end
+    puts "|---> Max Length=#{maxc} |---> Max Height #{maxr}}"
+    sleep(5)
   end
 end
