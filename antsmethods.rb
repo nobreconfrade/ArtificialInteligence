ee29fde49ce017575f1e43038769bfc7a0164bfc
William Pereira <willpereirabr@gmail.com>
2017-03-21 11:39:44 -0300

    better [show_grid] with minor mods and start of [dead_ants] [live_ants] method to create ants

--- antsmethods.rb
+++ antsmethods.rb
@@ -1,24 +1,31 @@
 class AntsMethods
-  def populate_grid(maxlenght,maxheight)
+  def dead_ants(grid)
+
+  end
+
+  def live_ants(grid)
+
+  end
+
+  def populate_grid(maxlength,maxheight)
     # NOTE: '0' for nothing
     # =>    '1' for live ants
     # =>    '2' for dead ants
     grid = Hash.new(" ")
-    for i in 0..maxlenght
-      for j in 0..maxheight
-        # grid[i,j] = 0
-      end
-    end
+    grid = dead_ants(grid)
+    grid = live_ants(grid)
     return grid
   end
-  def show_grid (grid,maxlenght,maxheight)
-    # NOTE: Next line clear the ubuntu terminal
+
+  def show_grid (grid,maxlength,maxheight)
+    # NOTE: Next line clear the ubuntu terminal and them wait X seconds
     system 'clear'
-    for i in 0...maxlenght do
+    sleep(5)
+    for i in 0...maxlength do
       for j in 0...maxheight do
         print "|"
-        print grid["#{i * maxlenght + j}"]
-        if j == maxlenght-1
+        print grid["#{i * maxlength + j}"]
+        if j == maxlength-1
           puts ""
         end
       end
