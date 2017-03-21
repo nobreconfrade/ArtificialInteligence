bf8d2450dfdc082e8af7e8f6fcd009452581bec8
William Pereira <willpereirabr@gmail.com>
2017-03-21 11:38:05 -0300

    length and height now fixed, only changeable by code

--- ants.rb
+++ ants.rb
@@ -2,12 +2,11 @@ require_relative "antsmethods.rb"
 
 class Ants < AntsMethods
 
-  maxlenght = 30
+  maxlength = 30
   maxheight = 30
   methods = AntsMethods.new
-  puts "Informe o tamanho da grid de formigas:"
-  puts "!UNDER CONSTRUCTION, 50 DEFAULT!"
-  grid = methods.populate_grid(maxlenght,maxheight)
-  methods.show_grid(grid,maxlenght,maxheight)
+  puts "|---> Max Length=#{maxlength} |---> Max Height #{maxheight}}"
+  grid = methods.populate_grid(maxlength,maxheight)
+  methods.show_grid(grid,maxlength,maxheight)
 
 end
