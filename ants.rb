82980ed04aea6da161559d6467a806a5e151d7c0
William Pereira <willpereirabr@gmail.com>
2017-03-22 11:38:09 -0300

    now with dinamic variables

--- ants.rb
+++ ants.rb
@@ -2,10 +2,17 @@ require_relative "antsmethods.rb"
 
 class Ants < AntsMethods
 
-  maxc = 30
-  maxr = 30
+  puts "Select the number of columns:"
+  maxc = gets.to_i
+  puts "Select the number of rows:"
+  maxr = gets.to_i
+  puts "Select the number of dead ants: (recommended: #{maxr*maxc/10})"
+  deadants = gets.to_i
+  puts "Select the number of ants alive: (recommended: #{maxr*maxc/100})"
+  aliveants = gets.to_i
+  grid = Hash.new(" ")
   methods = AntsMethods.new
-  grid = methods.populate_grid(maxr,maxc)
+  methods.populate_grid(grid,maxr,maxc,deadants,aliveants)
   methods.show_grid(grid,maxr,maxc)
 
 end
