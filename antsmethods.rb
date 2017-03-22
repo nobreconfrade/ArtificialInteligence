7750759b013aa52c99380cbcd898dc444f939e79
William Pereira <willpereirabr@gmail.com>
2017-03-22 11:39:21 -0300

    [alive_ants] and [dead_ants] working, need to make them walk

--- antsmethods.rb
+++ antsmethods.rb
@@ -1,57 +1,54 @@
 class AntsMethods
-  def dead_ants(grid,maxr,maxc)
-    deadants = 0
-    while (deadants != maxr*maxc/10)
-      key = rand(0..maxr) * maxr + rand(0..maxc)
-      if grid["#{key}"] == "2"
+  def dead_ants(grid,maxr,maxc,deadants)
+    count = 0
+    while (count != deadants)
+      key = rand(0...maxr) * maxr + rand(0...maxc)
+      if grid[key] == "*"
         next
       else
-        grid = {"#{key}" => "2"}
-        puts grid["#{key}"]
+        grid[key] = "*"
       end
-      deadants += 1
+      count += 1
     end
   end
 
-  def live_ants(grid)
-
+  def alive_ants(grid,maxr,maxc,aliveants)
+    count = 0
+    while (count != aliveants)
+      key = rand(0...maxr) * maxr + rand(0...maxc)
+      if grid[key] == "*" or grid[key] == "o"
+        next
+      else
+        grid[key] = "o"
+      end
+      count += 1
+    end
   end
 
-  def populate_grid(maxr,maxc)
+  def populate_grid(grid,maxr,maxc,deadants,aliveants)
     # NOTE: ' ' for nothing
-    # =>    '1' for live ants
-    # =>    '2' for dead ants
-    grid = Hash.new
-    for r in 0..maxr do
-      for c in 0..maxc do
-        key = r * maxr + c
-        grid = {"#{key}" => "0"}
-        puts grid["#{key}"]
-      end
-    end
-    show_grid(grid,maxr,maxc)
-    grid = {"450" => "2"}
-    show_grid(grid,maxr,maxc)
-    grid = dead_ants(grid,maxr,maxc)
-    # grid = live_ants(grid)
-    return grid
+    # =>    'o' for alive ants
+    # =>    '*' for dead ants
+    dead_ants(grid,maxr,maxc,deadants)
+    alive_ants(grid,maxr,maxc,aliveants)
   end
-  # FIXME: what is the problem with this method? It doesn't print the grid, it even "forgets" the values
+
   def show_grid (grid,maxr,maxc)
     # NOTE: Next line clear the ubuntu terminal and them wait X seconds
-    # system 'clear'
-    print grid["1"]
+    system 'clear'
+    # print grid.keys
     for r in 0...maxr do
+      print "|"
       for c in 0...maxc do
         key = r * maxr + c
+        print grid[key]
         print "|"
-        print grid["#{key}"]
         if c == maxc-1
-          puts ""
+          print "\n"
         end
       end
     end
     puts "|---> Max Length=#{maxc} |---> Max Height #{maxr}}"
-    sleep(5)
+    sleep(2)
   end
 end
