51c7d4ee4e3ef1b90596f38db28a82f95a3b7986
William Pereira <willpereirabr@gmail.com>
2017-03-27 15:14:36 -0300

    starting ants logic

--- ant.rb
+++ ant.rb
@@ -1,13 +1,14 @@
 class Ant
-  attr_accessor :ant_row, :ant_col, :ant_working
+  attr_accessor :ant_row, :ant_col, :ant_working, :ant_direction, :ant_steps
   # NOTE: '@@' means shared variable through objects
-  @@ant_total = 0
   def initialize(positionr, positionc, working)
     @ant_row = positionr
     @ant_col = positionc
     @ant_working = working
-    @@ant_total += 1
+    @ant_direction = rand(0..3)
+    @ant_steps = rand(1..10)
   end
+
   def to_s
     @str = "Row: "+@ant_row.to_s+"\n"
     @str += "Column: "+@ant_col.to_s+"\n"
@@ -16,8 +17,10 @@ class Ant
     else
       @str += "Working? No\n"
     end
-    @str += "Number of ant:"+@@ant_total.to_s+"\n"
+    @str += "Direction:"+@ant_direction.to_s+"\n"
+    @str += "Steps remaining:"+@ant_steps.to_s+"\n"
     @str += "---------------\n"
     @str
   end
+
 end
