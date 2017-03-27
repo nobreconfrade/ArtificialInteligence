66453d795afffbca1196b73ee9a2b46ff971c0be
William Pereira <willpereirabr@gmail.com>
2017-03-27 13:14:46 -0300

    showing the class and its objects, now commented

--- antcolony.rb
+++ antcolony.rb
@@ -1,5 +1,5 @@
 require_relative "antcolonymethods.rb"
-require_relative "ants.rb"
+require_relative "ant.rb"
 # PRAISE THE SUN
 #
 # made by: William Pereira
@@ -7,6 +7,7 @@ class AntColony < AntColonyMethods
 
   methods = AntColonyMethods.new
   grid = Hash.new(" ")
+  ants = Array.new
   count = 0
 
   puts "Select the number of columns:"
@@ -20,17 +21,25 @@ class AntColony < AntColonyMethods
   puts "An interaction is the full cycle of ants."
   puts "Chose the number of interactions you want:"
   interactions = gets.to_i
-  methods.populate_grid(grid,maxr,maxc,deadants,aliveants)
+  methods.populate_grid(grid,maxr,maxc,deadants,aliveants,ants)
   puts "Show all interactions? (y/n)"
   if gets == "y"
     while count != interactions
-      ant = Ants().new
       methods.show_grid(grid,maxr,maxc)
+
       count += 1
     end
   else
     # NOTE: This environment show the inicial board and the result.
+    # NOTE: How to interact in the ants object.
+    # ants.each do |ant|
+    #   puts ant.to_s
+    # end
     methods.show_grid(grid,maxr,maxc)
+    while count != interactions
+
+      count += 1
+    end
     methods.show_grid(grid,maxr,maxc)
   end
 end
