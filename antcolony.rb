c1164fff958ed413379c72b71238b37d5492c06f
William Pereira <willpereirabr@gmail.com>
2017-03-28 16:04:00 -0300

    ants are escaping

--- antcolony.rb
+++ antcolony.rb
@@ -22,18 +22,23 @@ class AntColony < AntColonyMethods
   puts "Chose the number of interactions you want:"
   interactions = gets.to_i
   methods.populate_grid(grid,maxr,maxc,deadants,aliveants,ants)
-  puts "Show all interactions? (y/n)"
-  if gets == "y"
+  puts "Show all interactions? (1 or 0)"
+  show = gets.to_i
+  if show == 1
     while count != interactions
-      methods.show_grid(grid,maxr,maxc)
+      methods.show_grid(grid,maxr,maxc,count)
       #NOTE: logic of ants
       ants.each do |ant|
         if ant.ant_working == 1
-          if grid[ant.ant_row * maxr + ant.ant_col]
-
-          end
+          methods.release_ants()
+          methods.walk_ants(ant,maxr,maxc,grid)
         else
-
+          if grid[ant.ant_row * maxr + ant.ant_col] == "@"
+            methods.capture_ants()
+            methods.walk_ants(ant,maxr,maxc,grid)
+          else
+            methods.walk_ants(ant,maxr,maxc,grid)
+          end
         end
       end
       count += 1
@@ -41,14 +46,14 @@ class AntColony < AntColonyMethods
   else
     # NOTE: This environment show the inicial board and the result.
     # NOTE: How to interact in the ants object.
-    ants.each do |ant|
-      puts ant.to_s
-    end
-    # methods.show_grid(grid,maxr,maxc)
-    # while count != interactions
-    #
-    #   count += 1
+    # ants.each do |ant|
+    #   puts ant.to_s
     # end
-    # methods.show_grid(grid,maxr,maxc)
+    methods.show_grid(grid,maxr,maxc,count)
+    while count != interactions
+
+      count += 1
+    end
+    methods.show_grid(grid,maxr,maxc,count)
   end
 end
