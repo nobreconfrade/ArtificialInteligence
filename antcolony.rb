23cc3f047357be5a3f213abf02ccaf368cc69be1
William Pereira <willpereirabr@gmail.com>
2017-03-28 22:33:09 -0300

    fixed and dinamic tests

--- antcolony.rb
+++ antcolony.rb
@@ -9,25 +9,35 @@ class AntColony < AntColonyMethods
   grid = Hash.new(" ")
   ants = Array.new
   count = 0
-  # lineofsight = 1
 
-  puts "Select the number of columns:"
-  maxc = gets.to_i
-  puts "Select the number of rows:"
-  maxr = gets.to_i
-  puts "Select the number of dead ants: (recommended: #{maxr*maxc/10})"
-  deadants = gets.to_i
-  puts "Select the number of ants alive: (recommended: #{maxr*maxc/100})"
-  aliveants = gets.to_i
-  puts "An interaction is the full cycle of ants."
-  puts "Chose the number of interactions you want:"
-  interactions = gets.to_i
+  # NOTE: Fixed tests
+  maxc = 100
+  maxr = 50
+  deadants = 500
+  aliveants = 50
+  interactions = 50000
+  lineofsight = 1
+  show = 0
+
+  #NOTE: Dinamic tests
+  # puts "Select the number of columns:"
+  # maxc = gets.to_i
+  # puts "Select the number of rows:"
+  # maxr = gets.to_i
+  # puts "Select the number of dead ants: (recommended: #{maxr*maxc/10})"
+  # deadants = gets.to_i
+  # puts "Select the number of ants alive: (recommended: #{maxr*maxc/100})"
+  # aliveants = gets.to_i
+  # puts "An interaction is the full cycle of ants."
+  # puts "Chose the number of interactions you want:"
+  # interactions = gets.to_i
+  # puts "The line of sight is the range that the ant can see and take an action over it."
+  # puts "Chose a line of sight:"
+  # lineofsight = gets.to_i
+  # puts "Show all interactions? (1 or 0)"
+  # show = gets.to_i
+
   methods.populate_grid(grid,maxr,maxc,deadants,aliveants,ants)
-  puts "The line of sight is the range that the ant can see and take an action over it."
-  puts "Chose a line of sight:"
-  lineofsight = gets.to_i
-  puts "Show all interactions? (1 or 0)"
-  show = gets.to_i
   if show == 1
     while count != interactions
       methods.show_grid(grid,maxr,maxc,count)
@@ -69,6 +79,7 @@ class AntColony < AntColonyMethods
         end
       end
       count += 1
+      puts " Interaction: #{count}"
     end
     methods.show_grid(grid,maxr,maxc,count-1)
   end
