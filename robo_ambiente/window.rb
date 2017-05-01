4d9f4cc00fa8eff3c7b27afa528fae8072afdbd0
William Pereira <willpereirabr@gmail.com>
2017-05-01 10:09:08 -0300

    initial and final position markers working

--- robo_ambiente/window.rb
+++ robo_ambiente/window.rb
@@ -8,6 +8,10 @@ array = Array.new
 size = 640
 offset_x = 0.0
 offset_y = 0.0
+initial_x = 0
+initial_y = 0
+final_x = 0
+final_y = 0
 tick = 0
 
 set title: "Robô busca cega"
@@ -24,38 +28,67 @@ File.open('mapa.txt','r') do |f|
   end
 end
 
+File.open('input.txt','r') do |f|
+  f.each_line do |c|
+    content = c.split(":")
+    if content[0] == "ix"
+      initial_x = content[1].to_i
+    end
+    if content[0] == "iy"
+      initial_y = content[1].to_i
+    end
+    if content[0] == "fx"
+      final_x = content[1].to_i.to_i
+    end
+    if content[0] == "fy"
+      final_y = content[1].to_i.to_i
+    end
+  end
+end
+
+# puts initial_x
+# puts initial_y
+# puts final_x
+# puts final_y
+
 j = 0
 i = 0
 
+for i in 0...42 do
+  for j in 0...42 do
+    if array[i*42+j] == 0
+      Square.new(offset_x,offset_y,size/42,'green')
+    elsif array[i*42+j] == 1
+      Square.new(offset_x,offset_y,size/42,'brown')
+    elsif array[i*42+j] == 2
+      Square.new(offset_x,offset_y,size/42,'blue')
+    elsif array[i*42+j] == 3
+      Square.new(offset_x,offset_y,size/42,'red')
+    else
+      Square.new(offset_x,offset_y,size/42,'white')
+    end
+    if i == initial_x and j == initial_y
+      Triangle.new(offset_x,offset_y,offset_x+size/42,offset_y,offset_x+(size/42)/2,offset_y+(size/42)/2,'white')
+    end
+    if i == final_x and j == final_y
+      Triangle.new(offset_x,offset_y,offset_x+size/42,offset_y,offset_x+(size/42)/2,offset_y+(size/42)/2,'black')
+    end
+    if j == 41
+      offset_y += size/42
+      offset_x = 0
+      next
+    end
+    offset_x += size/42
+  end
+end
 
 update do
   if tick % 60 == 0
     if tick < 120
-      for i in 0...42 do
-        for j in 0...42 do
-          if array[i*42+j] == 0
-            Square.new(offset_x,offset_y,size/42,'green')
-          elsif array[i*42+j] == 1
-            Square.new(offset_x,offset_y,size/42,'brown')
-          elsif array[i*42+j] == 2
-            Square.new(offset_x,offset_y,size/42,'blue')
-          elsif array[i*42+j] == 3
-            Square.new(offset_x,offset_y,size/42,'red')
-          else
-            Square.new(offset_x,offset_y,size/42,'white')
-          end
-          if j == 41
-            offset_y += size/42
-            offset_x = 0
-            next
-          end
-          offset_x += size/42
-        end
-      end
     else
     end
   end
-  puts tick
+  # puts tick
   tick += 1
 end
 
