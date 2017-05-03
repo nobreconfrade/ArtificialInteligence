af9747f99ef6b513e799e0f561e4b3aed4974bcd
William Pereira <willpereirabr@gmail.com>
2017-05-03 15:02:25 -0300

    working on (bfs) quality

--- robo_ambiente/bfs.rb
+++ robo_ambiente/bfs.rb
@@ -3,59 +3,104 @@ class Bfs
   def solver(pos_x,pos_y,array,final_x,final_y,initial_cost)
     @open = Array.new
     # @open.push(Magic.new(pos_x,pos_y,nil))
-    @open.push([pos_x,pos_y,initial_cost,nil])
+    @open.push([pos_x,pos_y,nil,nil])
     @closed = Array.new
     @array = array
     @prevlist = Array.new
     # @prevlist.push(Magic.new(pos_x,pos_y,nil))
-    @prevlist.push([pos_x,pos_y,initial_cost,nil])
+    @prevlist.push([pos_x,pos_y,nil,nil])
 
-
-    flag = 0
     count = 0
+    @finalarray = []
+
     while !@open.empty?
       @e = @open.first
-      @open.delete_at(0)
       @closed.push(@e)
+      @open.delete_at(0)
       # if @e.pos_x == final_x and @e.pos_y == final_y
-      if @e[0] == final_x and @e[1] == final_y
-        puts "HEY!"
-        flag = 1
-        break
-      end
       # if @e.pos_x - 1 >= 0
 
 #####################NOTE: TEST###########################
       if @e[0] - 1 >= 0
-        @el = [@e[0] - 1,@e[1],@e[2]+@array[@e[0]*42+@e[1]],@e[3]]
+        @el = [@e[0] - 1,@e[1],@e[0],@e[1]]
         unless @closed.include?(@el) and @open.include?(@el)
           @open.push(@el)
-          puts count
-          count += 1
+          @prevlist.push(@el)
+          if @el[0] == final_x and @el[1] == final_y
+            @prevlist.reverse_each do |c|
+              puts "LISTEN!1"
+              if c[0] == prev_x and c[1] == prev_y
+                count += array[c[0]*42+c[1]]
+                prev_x = c[2]
+                prev_y = c[3]
+                @finalarray.push([c[0],c[1]])
+              end
+            end
+            puts @finalarray
+            return count,@finalarray,@closed
+          end
         end
       end
       if @e[1] + 1 < 42
-        @el = [@e[0],@e[1] + 1, @e[2] + @array[@e[0]*42+@e[1]], @e[3]]
+        @el = [@e[0],@e[1] + 1, @e[0], @e[1]]
         unless @closed.include?(@el) and @open.include?(@el)
           @open.push(@el)
-          puts count
-          count += 1
+          @prevlist.push(@el)
+          if @el[0] == final_x and @el[1] == final_y
+            @prevlist.reverse_each do |c|
+              puts "LISTEN!2"
+              if c[0] == prev_x and c[1] == prev_y
+                count += array[c[0]*42+c[1]]
+                prev_x = c[2]
+                prev_y = c[3]
+                @finalarray.push([c[0],c[1]])
+              end
+            end
+            puts @finalarray
+            return count,@finalarray,@closed
+          end
         end
       end
       if @e[0] + 1 < 42
-        @el = [@e[0] + 1,@e[1], @e[2] + @array[@e[0]*42+@e[1]], @e[3]]
+        @el = [@e[0] + 1,@e[1], @e[0], @e[1]]
         unless @closed.include?(@el) and @open.include?(@el)
           @open.push(@el)
-          puts count
-          count += 1
+          @prevlist.push(@el)
+          if @el[0] == final_x and @el[1] == final_y
+            prev_x = @el[2]
+            prev_y = @el[3]
+            @prevlist.reverse_each do |c|
+              # puts "LISTEN!3"
+              if c[0] == prev_x and c[1] == prev_y
+                count += array[c[0]*42+c[1]]
+                prev_x = c[2]
+                prev_y = c[3]
+                @finalarray.push([c[0],c[1]])
+              end
+            end
+            # print @finalarray
+            return count,@finalarray,@closed
+          end
         end
       end
       if @e[1] - 1 >= 0
-        @el = [@e[0],@e[1] - 1, @e[2] + @array[@e[0]*42+@e[1]], @e[3]]
+        @el = [@e[0],@e[1] - 1, @e[0], @e[1]]
         unless @closed.include?(@el) and @open.include?(@el)
           @open.push(@el)
-          puts count
-          count += 1
+          @prevlist.push(@el)
+          if @el[0] == final_x and @el[1] == final_y
+            @prevlist.reverse_each do |c|
+              puts "LISTEN!4"
+              if c[0] == prev_x and c[1] == prev_y
+                count += array[c[0]*42+c[1]]
+                prev_x = c[2]
+                prev_y = c[3]
+                @finalarray.push([c[0],c[1]])
+              end
+            end
+            puts @finalarray
+            return count,@finalarray,@closed
+          end
         end
       end
 
@@ -89,11 +134,7 @@ class Bfs
       #   end
       # end
     end
-    if flag == 1
-      # return @el.cost,@closed.length
-      return @el[2],@closed.length
-    end
-    puts "Sem solução =/"
-    puts "#{@closed}"
+
+
   end
 end
