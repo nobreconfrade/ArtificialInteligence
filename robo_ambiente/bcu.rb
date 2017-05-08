85e513103fa5a48e7802295f82010516b68fb989
William Pereira <willpereirabr@gmail.com>
2017-05-07 21:47:44 -0300

    BCU with this annoying bug on [1,1,1]

--- robo_ambiente/bcu.rb
+++ robo_ambiente/bcu.rb
@@ -1,48 +1,87 @@
 class Bcu
-  def initialize
-    @exparray = []
-    @distarray = Array.new(42**2,9999999)
+  def solver(initial_x,initial_y,array,final_x,final_y)
+    @array = array
+    @dist = Array.new(@array.length,Float::INFINITY)
+    @dist[initial_x*42+initial_y] = 0
+    @prev = []
+    @pq = []
+    @pq.push([initial_x,initial_y,0])
 
-  end
-  def solver
-  end
 
-  # def initialize(array)
-  #   @array = array
-  #   dist = Array.new(array.length,99999)
-  #   prev = Array.new(array.length,-1)
-  #   vcount = 0
-  #   count = 0
-  #   qarray = {}
-  # end
-  # # @array.each do |v|
-  # #   vcount += 1
-  # #   qarray << v
-  # # end
-  #
-  # dist[0] = 0
-  # qarray[0] = {0, dist[0]}
-  # while prev[42*42] != -1 or !qarray.null?
-  #   first = qarray[0]
-  #   qarray.delete_if{i==0}
-  #   count+=1
-  #   if count < 42*42
-  #     qarray << @array[count]
-  #     qarray[count] = {}
-  #     qarray << {first[0], dist[first] + @array[count] == 0 ? 1: @array[count] == 1? 5: @array[count] == 2? 10: 15
-  #   end
-  #   if count < 42*42
-  #     qarray << @array[count+42]
-  #     qarray << {count, dist[first] + @array[count] == 0 ? 1: @array[count] == 1? 5: @array[count] == 2? 10: 15
-  #   end
-  #   if count-1 >= 0
-  #     qarray << @array[count-1]
-  #     qarray << {count, dist[first] + @array[count] == 0 ? 1: @array[count] == 1? 5: @array[count] == 2? 10: 15
-  #   end
-  #   if count-42 >= 0
-  #     qarray << @array[count-42]
-  #     qarray << {count, dist[first] + @array[count] == 0 ? 1: @array[count] == 1? 5: @array[count] == 2? 10: 15
-  #   end
-  # end
+    help = 0
+    nc = 0
+    ec = 0
+    sc = 0
+    wc = 0
+
+
+    while @pq.any?
+      @pq = @pq.sort_by {|key1,key2,val| val }
+      if help < 5
+        print "#{@pq}\n"
+        puts "-----------------------------------"
+        help += 1
+      end
+      @e = @pq.shift
+
+      if @e[0] == final_x and @e[1] == final_y
+        puts "HEY!"
+        return 0,0,0
+      end
+
+      if @e[0] - 1 >= 0
+        if @dist[@e[0] - 1 * 42 + @e[1]] > @dist[@e[0] * 42 + @e[1]] + @array[@e[0] - 1 * 42 + @e[1]]
+          nc += 1
+          @dist[@e[0] - 1 * 42 + @e[1]] = @dist[@e[0] * 42 + @e[1]] + @array[@e[0] - 1 * 42 + @e[1]]
+          @prev[@e[0] - 1 * 42 + @e[1]] = [@e[0],@e[1]]
+          @pq.push([@e[0] - 1, @e[1],@dist[@e[0] - 1 * 42 + @e[1]]])
+        end
+      end
+
+      if @e[1] + 1 < 42
+        if @dist[@e[0] * 42 + @e[1] + 1] > @dist[@e[0] * 42 + @e[1]] + @array[@e[0] * 42 + @e[1] + 1]
+          ec += 1
+          @dist[@e[0] * 42 + @e[1] + 1] = @dist[@e[0] * 42 + @e[1]] + @array[@e[0] * 42 + @e[1] + 1]
+          @prev[@e[0] * 42 + @e[1] + 1] = [@e[0],@e[1]]
+          @pq.push([@e[0], @e[1] + 1,@dist[@e[0] * 42 + @e[1] + 1]])
+        end
+      end
+
+      if @e[0] + 1 < 42
+        ################## NOTE: HEAVY TEST ENV #############################
+        # print "dist:#{@dist[@e[0] + 1 * 42 + @e[1]]}\n"
+        # print "new:#{@dist[@e[0] * 42 + @e[1]]}\n"
+        # print "new:#{@e[0]} e #{@e[1]}\n"
+        if @e[0] == 1 and @e[1] == 1
+          print "dist:#{@dist[@e[0] + 1 * 42 + @e[1]]}\n"
+          print "atual:#{@dist[@e[0] * 42 + @e[1]]}\n"
+          print "array:#{@array[@e[0] + 1 * 42 + @e[1]]}\n"
+          print "prev do dist:#{@prev[@e[0] + 1 * 42 + @e[1]]}\n"
+        end
+        if @dist[@e[0] + 1 * 42 + @e[1]] > @dist[@e[0] * 42 + @e[1]] + @array[@e[0] + 1 * 42 + @e[1]]
+          sc += 1
+          @dist[@e[0] + 1 * 42 + @e[1]] = @dist[@e[0] * 42 + @e[1]] + @array[@e[0] + 1 * 42 + @e[1]]
+          @prev[@e[0] + 1 * 42 + @e[1]] = [@e[0],@e[1]]
+          @pq.push([@e[0] + 1, @e[1],@dist[@e[0] + 1 * 42 + @e[1]]])
+        end
+      end
+
+      if @e[1] - 1 >= 0
+        if @dist[@e[0] * 42 + @e[1] - 1] > @dist[@e[0] * 42 + @e[1]] + @array[@e[0] * 42 + @e[1] - 1]
+          wc += 1
+          @dist[@e[0] * 42 + @e[1] - 1] = @dist[@e[0] * 42 + @e[1]] + @array[@e[0] * 42 + @e[1] - 1]
+          @prev[@e[0] * 42 + @e[1] - 1] = [@e[0],@e[1]]
+          @pq.push([@e[0], @e[1] - 1,@dist[@e[0] * 42 + @e[1] - 1]])
+        end
+      end
+
+    end
+    # puts "North counter: #{nc}"
+    # puts "East counter: #{ec}"
+    # puts "South counter: #{sc}"
+    # puts "West counter: #{wc}"
+    # print "#{@dist[1*42+1]}\n"
+    # print "#{@dist[2*42+1]}\n"
+  end
 
 end
--- robo_ambiente/window.rb
+++ robo_ambiente/window.rb
@@ -103,56 +103,57 @@ visitednodes = []
 expnodes = []
 finalexpnodes = []
 ################################# NOTE: Select algorith: #################################
-cost,visitednodes,expnodes= bfs.solver(initial_x,initial_y,array,final_x,final_y,array[initial_x,initial_y][0])
-expnodes.each do |c|
-  unless finalexpnodes.include?([c[0],c[1]])
-    finalexpnodes.push([c[0],c[1]])
-  end
-end
+# cost,visitednodes,expnodes = bfs.solver(initial_x,initial_y,array,final_x,final_y,array[initial_x,initial_y][0])
+cost,visitednodes,expnodes = bcu.solver(initial_x,initial_y,array,final_x,final_y)
+# expnodes.each do |c|
+#   unless finalexpnodes.include?([c[0],c[1]])
+#     finalexpnodes.push([c[0],c[1]])
+#   end
+# end
 
 
 update do
   if tick % 60 == 0
     if tick == 240
 
+      #
+      # for i in 0...42 do
+      #   for j in 0...42 do
+      #     if array[i*42+j] == 1
+      #       Square.new(offset_x,offset_y,size/42,'green')
+      #     elsif array[i*42+j] == 5
+      #       Square.new(offset_x,offset_y,size/42,'brown')
+      #     elsif array[i*42+j] == 10
+      #       Square.new(offset_x,offset_y,size/42,'blue')
+      #     elsif array[i*42+j] == 15
+      #       Square.new(offset_x,offset_y,size/42,'red')
+      #     else
+      #       Square.new(offset_x,offset_y,size/42,'white')
+      #     end
+      #     if finalexpnodes.include?([i,j])
+      #       Square.new(offset_x,offset_y,size/42,[0.0, 0.0, 0.0, 0.5])
+      #     end
+      #     if visitednodes.include?([i,j])
+      #       Square.new(offset_x,offset_y,size/42,[1.0, 0.64, 0.0, 0.5])
+      #     end
+      #     if i == initial_x and j == initial_y
+      #       Triangle.new(offset_x,offset_y,offset_x+size/42,offset_y,offset_x+(size/42)/2+0.5,offset_y+(size/42),'white')
+      #     end
+      #     if i == final_x and j == final_y
+      #       Triangle.new(offset_x,offset_y,offset_x+size/42,offset_y,offset_x+(size/42)/2+0.5,offset_y+(size/42),'black')
+      #     end
+      #     if j == 41
+      #       offset_y += size/42
+      #       offset_x = 0
+      #       next
+      #     end
+      #     offset_x += size/42
+      #   end
+      # end
 
-      for i in 0...42 do
-        for j in 0...42 do
-          if array[i*42+j] == 1
-            Square.new(offset_x,offset_y,size/42,'green')
-          elsif array[i*42+j] == 5
-            Square.new(offset_x,offset_y,size/42,'brown')
-          elsif array[i*42+j] == 10
-            Square.new(offset_x,offset_y,size/42,'blue')
-          elsif array[i*42+j] == 15
-            Square.new(offset_x,offset_y,size/42,'red')
-          else
-            Square.new(offset_x,offset_y,size/42,'white')
-          end
-          if finalexpnodes.include?([i,j])
-            Square.new(offset_x,offset_y,size/42,[0.0, 0.0, 0.0, 0.5])
-          end
-          if visitednodes.include?([i,j])
-            Square.new(offset_x,offset_y,size/42,[1.0, 0.64, 0.0, 0.5])
-          end
-          if i == initial_x and j == initial_y
-            Triangle.new(offset_x,offset_y,offset_x+size/42,offset_y,offset_x+(size/42)/2+0.5,offset_y+(size/42),'white')
-          end
-          if i == final_x and j == final_y
-            Triangle.new(offset_x,offset_y,offset_x+size/42,offset_y,offset_x+(size/42)/2+0.5,offset_y+(size/42),'black')
-          end
-          if j == 41
-            offset_y += size/42
-            offset_x = 0
-            next
-          end
-          offset_x += size/42
-        end
-      end
-
-      puts "Custo: #{cost}"
-      puts "Nós visitados no caminho: #{visitednodes.length}"
-      puts "Nós visitados no total: #{finalexpnodes.length}"
+      # puts "Custo: #{cost}"
+      # puts "Nós visitados no caminho: #{visitednodes.length}"
+      # puts "Nós visitados no total: #{finalexpnodes.length}"
 
     else
     end
